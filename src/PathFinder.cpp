bf631485faa8d5035319f2e062f7b84fe9fc9dd9
Dimitar Dimitrov <dimitar.dimitrov@student.manchester.ac.uk>
2018-03-01 21:27:25 +0000

    Initial implementation of the path finding algorithm

---
+++ src/PathFinder.cpp
@@ -0,0 +1,98 @@
+//
+// Created by dimitar on 01.03.18.
+//
+
+#include "PathFinder.hpp"
+
+
+PathFinder::PathFinder(const matrix m)
+{
+    /*
+     *
+     * THIS MIGHT NEED CHANGING AS IT WILL BE TOO SLOW TO COPY OER MEMORY ALL THE TIME
+     *
+     */
+    std::copy(&m[0][0], &m[0][0] + MATRIX_DIM * MATRIX_DIM, &initial_rates_[0][0]);
+
+    for (int i = 1; i < MATRIX_DIM; ++i)
+        for (int j = 0; j < MATRIX_DIM; ++j)
+            for (int k = 0; k < MATRIX_DIM; ++k)
+                benefits_[i][j][k] = 0;
+}
+
+void PathFinder::initialize()
+{
+    for (short i = 0; i < MATRIX_DIM; ++i)
+    {
+        for (short j = 0; j < MATRIX_DIM; ++j)
+        {
+            //the most affordable path of 1 form i to j is to go directly
+            if (initial_rates_[i][j] > 0)
+                path_[1][i][j] = j;
+            benefits_[0][i][j] = initial_rates_[i][j];
+        }
+    }
+}
+
+void PathFinder::SetRates(const matrix &m)
+{
+    std::copy(&m[0][0], &m[0][0] + MATRIX_DIM * MATRIX_DIM, &initial_rates_[0][0]);
+}
+
+std::vector<short> PathFinder::GetPath()
+{
+    double max_profit = 0;
+    short max_profit_len = 0, max_profit_start = 0;
+
+    for (short path_len = 1; path_len < MATRIX_DIM; ++path_len)
+    {
+        for (short from = 0; from < MATRIX_DIM; ++from)
+        {
+            for (short to = 0; to < MATRIX_DIM; ++to)
+            {
+                for (short through = 0; through < MATRIX_DIM; ++through)
+                {
+                    double possible_benefit = initial_rates_[from][through] * benefits_[path_len - 1][through][to];
+                    double* current_benefit = &benefits_[path_len][from][to];
+
+                    if (*current_benefit < possible_benefit)
+                    {
+                        *current_benefit = possible_benefit;
+                        path_[path_len][from][to] = through;
+
+                        if (max_profit < benefits_[path_len][from][from])
+                        {
+                            max_profit = benefits_[path_len][from][from];
+                            max_profit_len = path_len;
+                            max_profit_start = from;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return PathFinder::ConstructPath(max_profit_len, max_profit_start);
+}
+
+std::string PathFinder::GetStrPath()
+{
+    return std::string();
+}
+
+std::vector<short> PathFinder::ConstructPath(short x, short from) {
+    std::vector<short> path_to_return(x);
+
+    short path_len = x, to = from;
+
+    while (path_len > 0)
+    {
+        path_to_return.push_back(from);
+        from = path_[path_len][from][to];
+        path_len--;
+    }
+
+    std::reverse(path_to_return.begin(), path_to_return.end());
+
+    return path_to_return;
+}
--- src/PathFinder.hpp
+++ src/PathFinder.hpp
@@ -5,10 +5,26 @@
 #ifndef BOOSTHTTP_PATHFINDER_HPP
 #define BOOSTHTTP_PATHFINDER_HPP
 
+#include "matrix.hpp"
+#include <string>
+#include <vector>
+#include <algorithm>
+
+typedef short path[MATRIX_DIM][MATRIX_DIM][MATRIX_DIM];
+
 class PathFinder
 {
 public:
-    PathFiner();
+    PathFinder(const matrix m);
+    void SetRates(const matrix& m);
+    std::vector<short> GetPath();
+    std::string GetStrPath();
+    void initialize();
+private:
+    path path_;
+    matrix initial_rates_;
+    matrix benefits_[MATRIX_DIM];
+    std::vector<short> ConstructPath(short x, short y);
 };
 
 #endif //BOOSTHTTP_PATHFINDER_HPP
