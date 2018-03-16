a9a479794e4bdfed6abaa5d02ae0570d6036fe88
Konstantin Vladimirov <konstantin.vladimirov@student.manchester.ac.uk>
2018-03-16 21:09:09 +0000

    Adapted ExchangConnection to work with the BattleFX exchange.

--- src/ExchangeConnection.cpp
+++ src/ExchangeConnection.cpp
@@ -4,72 +4,68 @@
 
 #include <boost/log/trivial.hpp>
 #include "ExchangeConnection.hpp"
+#include <boost/lexical_cast.hpp>
+#include <iostream>
+using tcp = boost::asio::ip::tcp;
+namespace http = boost::beast::http;
 
-using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
-namespace http = boost::beast::http;    // from <boost/beast/http.hpp>
-
-ExchangeConnection::ExchangeConnection(std::string host, std::string port, std::string target)
-        : host_(host), port_(port), target_(target), resolver_(ioc_), socket_(ioc_) {
+ExchangeConnection::ExchangeConnection(const std::string& host, const std::string& port,  const std::string& apiKey)
+        : host_(host), port_(port), apiKey_(apiKey), resolver_(ioc_), socket_(ioc_)
+{
+    buildRequests();
 }
 
-boost::system::error_code ExchangeConnection::establishConnection() {
+boost::system::error_code ExchangeConnection::establishConnection()
+{
     try {
         auto const results = resolver_.resolve(host_, port_);
 
         if (results.empty()) {
-            //BOOST_LOG_TRIVIAL(error) << "Can't resolve host :" << host_ << "\n";
+            BOOST_LOG_TRIVIAL(error) << "Can't resolve host :" << host_ << "\n";
             return boost::asio::error::host_not_found;
         }
-
-
-        boost::asio::socket_base::keep_alive keep_alive_option(true);
-        //socket_.set_option(keep_alive_option);
         boost::asio::connect(socket_, results.begin(), results.end());
-
+        socket_.set_option(tcp::no_delay(true));
+        socket_.set_option(boost::asio::socket_base::keep_alive(true));
         if (!socket_.is_open()) {
-            //BOOST_LOG_TRIVIAL(info) << "Error opening socket to " << host_ << " :" << port_ << "\n";
+            BOOST_LOG_TRIVIAL(info) << "Error opening socket to " << host_ << " :" << port_ << "\n";
             return boost::asio::error::broken_pipe;
         }
-        //BOOST_LOG_TRIVIAL(info) << "Opened socket to " << host_ << " :" << port_ << "\n";
+        BOOST_LOG_TRIVIAL(info) << "Opened socket to " << host_ << " :" << port_ << "\n";
 
-//
-//        http::request<http::string_body> req{http::verb::get, target_, 11};
-//        req.set(http::field::host, host_);
-//        req.set(http::field::user_agent, "Botty");
-//        req.set(http::field::keep_alive, true);
-//        //Send an http request to keep the connection alive
-//        http::write(socket_, req);
-//
-//        //Buffer to store the reply in
-//        boost::beast::flat_buffer response_buffer;
-//        http::response<http::string_body> response;
-//        http::read(socket_, response_buffer, response);
-//
-//        BOOST_LOG_TRIVIAL(info) << "Got response from server \n"
-//                                << response.body();
-//
     } catch (boost::system::system_error &e) {
-        //BOOST_LOG_TRIVIAL(error) << "Exception in establish connection " << e.what() << "\n";
+        BOOST_LOG_TRIVIAL(error) << "Exception in establishing connection to exchange" << e.what() << "\n";
         return boost::asio::error::not_connected;
     }
     return boost::system::error_code{};
 }
 
-boost::system::error_code ExchangeConnection::getBalanceJson(std::string &strJson) {
-
+boost::system::error_code ExchangeConnection::getBalanceJson(std::string &strJson)
+{
     try {
-        http::request<http::string_body> req{http::verb::post, target_, 11};
-
-        req.set(http::field::host, host_);
-        req.set(http::field::user_agent, "Botty");
-        req.set(http::field::keep_alive, true);
-        //Set the request body
-        req.body() = "GIVE ME MY BALANCE!!!!";
-        //Calculate the body size for the header
-        req.prepare_payload();
-
         //Send an http request to keep the connection alive
-        http::write(socket_, req);
+        http::write(socket_, getBalanceRequest_);
+        std::cout <<getBalanceRequest_ << std::endl;
+        //Buffer to store the reply in
+        boost::beast::flat_buffer response_buffer;
+        http::response<http::string_body> response;
+
+        http::read(socket_, response_buffer, response);
+
+        //BOOST_LOG_TRIVIAL(info) << "Got balance from server \n"
+        //                        << response.body();
+        strJson = response.body();
+    } catch (boost::system::system_error &e) {
+        BOOST_LOG_TRIVIAL(error) << "Exception in get balance " << e.what() << "\n";
+        return boost::asio::error::not_connected;
+    }
+    return boost::system::error_code{};
+}
+boost::system::error_code ExchangeConnection::getMarketJson(std::string& strJson)
+{
+    try {
+        //Send an http request to keep the connection alive
+        http::write(socket_, getMarketRequest_);
 
         //Buffer to store the reply in
         boost::beast::flat_buffer response_buffer;
@@ -79,104 +75,60 @@ boost::system::error_code ExchangeConnection::getBalanceJson(std::string &strJso
 
         //BOOST_LOG_TRIVIAL(info) << "Got balance from server \n"
         //                        << response.body();
-        strJson = std::move(response.body());
-    } catch (boost::system::system_error &e) {
-        //BOOST_LOG_TRIVIAL(error) << "Exception in get balance " << e.what() << "\n";
+        strJson = response.body();
+    } catch (boost::system::system_error& e) {
+        //BOOST_LOG_TRIVIAL(error) << "Exception in get  " << e.what() << "\n";
         return boost::asio::error::not_connected;
     }
     return boost::system::error_code{};
 }
+boost::system::error_code ExchangeConnection::sendOrder(const std::string& pair, double amount, std::string& responseJson)
+{
+    boost::beast::http::request<boost::beast::http::string_body> orderRequest;
+    orderRequest.method(http::verb::get);
+    orderRequest.version(11);
+    //GET /trade/<api_key>/<buy|sell>/<ccy_pair>/<amount>
+    //std::cout <<"Sending order with amount " << amount << "   and amountStr= " << boost::lexical_cast<std::string>(amount) << std::endl;
+    std::string amountStr = std::move(boost::lexical_cast<std::string>(amount));
 
-boost::system::error_code ExchangeConnection::getTickerJsonCached(int tickerId, std::string &strJson) {
     try {
-        //Send an http request to keep the connection alive
-        http::write(socket_, cached_requests_[tickerId]);
+        orderRequest.target("/trade/" + apiKey_ + "/sell/" + pair + "/" + amountStr);
+        orderRequest.set(http::field::host, host_);
+        orderRequest.set(http::field::user_agent, "Botty");
 
-        //Buffer to store the reply in
+        //BOOST_LOG_TRIVIAL(info) << "Making order request :\n" << orderRequest << std::endl;
+
+
+        http::write(socket_, orderRequest);
+
         boost::beast::flat_buffer response_buffer;
         http::response<http::string_body> response;
-
         http::read(socket_, response_buffer, response);
+        //BOOST_LOG_TRIVIAL(info) << "OrderResponse" << response.body() << "\n";
 
-        //BOOST_LOG_TRIVIAL(info) << "Got tickers from server \n"
-        //                        << response.body();
-        strJson = std::move(response.body());
 
-    } catch (boost::system::system_error &e) {
-        //BOOST_LOG_TRIVIAL(error) << "Exception in get ticker " << e.what() << "\n";
+        responseJson = response.body();
+    }catch(std::exception& e)
+    {
+        BOOST_LOG_TRIVIAL(error) << "Exception in send order  " << e.what() << "\n";
         return boost::asio::error::not_connected;
     }
-    return boost::system::error_code{};
+
+
 }
+void ExchangeConnection::buildRequests()
+{
+    getMarketRequest_.method(http::verb::get);
+    getMarketRequest_.keep_alive(true);
+    getMarketRequest_.version(11);
+    getMarketRequest_.target("/market");
+    getMarketRequest_.set(http::field::host, host_);
+    getMarketRequest_.set(http::field::user_agent, "Botty");
 
-boost::system::error_code ExchangeConnection::getTickerJson(const std::string &ticker, std::string &strJson) {
-    try {
-        http::request<http::string_body> req{http::verb::get, target_ + "?base=" + ticker, 11};
-        req.set(http::field::host, host_);
-        req.set(http::field::user_agent, "Botty");
-        req.set(http::field::keep_alive, true);
-
-
-        //Send an http request to keep the connection alive
-        http::write(socket_, req);
-
-        //Buffer to store the reply in
-        boost::beast::flat_buffer response_buffer;
-        http::response<http::string_body> response;
-
-        http::read(socket_, response_buffer, response);
-
-        BOOST_LOG_TRIVIAL(info) << "Got tickers from server \n" << response.body();
-
-        strJson = std::move(response.body());
-
-    } catch (boost::system::system_error &e) {
-        //BOOST_LOG_TRIVIAL(error) << "Exception in get ticker " << e.what() << "\n";
-        return boost::asio::error::not_connected;
-    }
-    return boost::system::error_code{};
-}
-
-
-boost::system::error_code ExchangeConnection::sendOrder(std::string_view ordJson, std::string &responceJson) {
-    try {
-        http::request<http::string_body> req{http::verb::post, target_, 11};
-        req.set(http::field::host, host_);
-        req.set(http::field::user_agent, "Botty");
-        req.set(http::field::keep_alive, true);
-        //Set the request body
-        req.body() = ordJson;
-        //Calculate the body size for the header
-        req.prepare_payload();
-
-        //Send an http request to keep the connection alive
-        http::write(socket_, req);
-
-        //Buffer to store the reply in
-        boost::beast::flat_buffer response_buffer;
-        http::response<http::string_body> response;
-
-
-        http::read(socket_, response_buffer, response);
-
-        //BOOST_LOG_TRIVIAL(info) << "Got made order to server \n"
-        //                        << response.body();
-        responceJson = std::move(response.body());
-    } catch (boost::system::system_error &e) {
-        //BOOST_LOG_TRIVIAL(error) << "Exception in send order " << e.what() << "\n";
-        return boost::asio::error::not_connected;
-    }
-    return boost::system::error_code{};
-}
-
-
-void ExchangeConnection::cacheRequests(const std::vector<std::string> &tickers) {
-    //https://api.fixer.io/latest?base=USD
-    for (const std::string &ticker: tickers) {
-        http::request<http::string_body> req{http::verb::get, target_ + "?base=" + ticker, 11};
-        req.set(http::field::host, host_);
-        req.set(http::field::user_agent, "Botty");
-        req.set(http::field::keep_alive, true);
-        cached_requests_.push_back(req);
-    }
+    getBalanceRequest_.method(http::verb::get);
+    getBalanceRequest_.keep_alive(true);
+    getBalanceRequest_.version(11);
+    getBalanceRequest_.target("/account/" + apiKey_);
+    getBalanceRequest_.set(http::field::user_agent, "Botty");
+    getBalanceRequest_.set(http::field::host, host_);
 }
--- src/ExchangeConnection.hpp
+++ src/ExchangeConnection.hpp
@@ -1,73 +1,44 @@
 //
 // Created by konstantin on 27/02/18.
 //
+
+#include <boost/asio.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/system/error_code.hpp>
+
 #include <string>
 #include <string_view>
 #include <functional>
 #include <thread>
 #include <atomic>
 
-#include <boost/asio.hpp>
-#include <boost/beast/core.hpp>
-#include <boost/asio/spawn.hpp>
-#include <boost/beast/http.hpp>
-#include <boost/system/error_code.hpp>
-
-
-#include <boost/chrono.hpp>
-#include <boost/thread/thread.hpp>
-
-#include <iostream>
-
 class ExchangeConnection
 {
+    void buildRequests();
 public:
-    ExchangeConnection(std::string host, std::string port, std::string target);
+    ExchangeConnection(const std::string& host, const std::string& port, const std::string& apiKey);
     ExchangeConnection(const ExchangeConnection& that) = delete;
     ExchangeConnection&operator=(ExchangeConnection const&) = delete;
-    boost::system::error_code getTickerJson(const std::string& ticker, std::string& strJson);
-    template<typename Func>
-    boost::system::error_code getTickersBatch(Func func){
-        using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
-        namespace http = boost::beast::http;    // from <boost/beast/http.hpp>
-
-        
-        boost::system::error_code ec;
-        for(auto& req : cached_requests_)
-        {
-            http::write(socket_, req, ec);
-            if(ec)
-            {
-                return ec;
-            }
-        }
-        boost::beast::flat_buffer response_buffer;
-        for(auto _: cached_requests_)
-        {
-            http::response<http::string_body> response;
-
-            http::read(socket_, response_buffer, response);
-            func(response.body());
-        }
 
 
-        return boost::system::error_code();
-    }
-    boost::system::error_code getBalanceJson(std::string& strJson);
-    boost::system::error_code sendOrder(std::string_view ordJson, std::string& responceJson);
     boost::system::error_code establishConnection();
-
-    boost::system::error_code getTickerJsonCached(int tickerId, std::string &strJson);
-    void cacheRequests(const std::vector<std::string>& tickers);
+    boost::system::error_code getBalanceJson(std::string& strJson);
+    boost::system::error_code getMarketJson(std::string& strJson);
+    boost::system::error_code sendOrder(const std::string& pair, double amount, std::string &responceJson);
     bool isOpen();
 private:
-    std::vector<boost::beast::http::request<boost::beast::http::string_body>> cached_requests_;
     std::string host_;
     std::string port_;
-    std::string target_;
+    std::string apiKey_;
     boost::asio::io_context ioc_;
     boost::asio::ip::tcp::resolver resolver_;
     boost::asio::ip::tcp::socket socket_;
+
+    std::vector<boost::beast::http::request<boost::beast::http::string_body>> cached_requests_;
+    boost::beast::http::request<boost::beast::http::string_body> getBalanceRequest_;
+    boost::beast::http::request<boost::beast::http::string_body> getMarketRequest_;
+
 };
 
 
